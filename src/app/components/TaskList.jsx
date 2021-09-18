39af2e15f71269eeb3fee58e66bebd33c4fa545f
Marko Popovic <m4rk0p0p@gmail.com>
2021-09-18 09:59:03 +0200

    Add a button for creation of a new task

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { connect } from 'react-redux' 
+import { connect } from 'react-redux'
+import { requestTaskCreation } from '../store/mutations'
 
-export const TaskList = ({tasks, name})=>(
+export const TaskList = ({tasks, name, id, createNewTask})=>(
     <div>
         <h3>
             {name}
         </h3>
         <div>
-            {tasks.map(task=>(<div>{task.name}</div>))}
+            {tasks.map(task=>(<div key={task.id}>{task.name}</div>))}
         </div>
+        <button onClick={()=>createNewTask(id)}>Add new</button>
     </div>
 )
 
@@ -22,4 +24,13 @@ const mapStateToProps = (state, ownProps)=>{
     }
 };
 
-export const ConnectedTaskList = connect(mapStateToProps)(TaskList);
+const mapDispatchToProps = (dispatch, ownProps)=>{
+    return {
+        createNewTask(id) {
+            console.log("Creating new task...", id);
+            dispatch(requestTaskCreation(id));
+        }
+    }
+}
+
+export const ConnectedTaskList = connect(mapStateToProps, mapDispatchToProps)(TaskList);
