574cf602df19bdda319a1c98ff15c7599ff35f68
Marko Popovic <m4rk0p0p@gmail.com>
2021-09-19 07:14:23 +0200

    Add a skeleton for a task creation saga

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,10 +1,22 @@
 import { createStore, applyMiddleware } from "redux";
 import { defaultState } from "../../server/defaultState";
 import { createLogger } from "redux-logger";
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleware = createSagaMiddleware();
+import * as sagas from "./sagas.mock";
 
 export const store = createStore(
     function reducer(state = defaultState, action) {
         return state;
     },
-    applyMiddleware(createLogger())
-)
+    applyMiddleware(createLogger(), sagaMiddleware)
+);
+
+for (let saga in sagas) {
+    // Importing from mock sagas gives an object (or array?) holding
+    // all sagas. Then iterating through those with for each loop
+    // returns just a name of the saga, but not the saga itself?!
+    // Gotta love javascript.
+    sagaMiddleware.run(sagas[saga])
+}
