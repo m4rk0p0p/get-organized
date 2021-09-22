65f98526ed12d8a5fa44660f1f8c5789497d8801
Marko Popovic <m4rk0p0p@gmail.com>
2021-09-22 21:17:01 +0200

    Create a script that intializes a database with default state
    * Implement creation of a mongoDB database
    * Populate that database with content from defaultState.js

---
+++ src/server/connect-db.js
@@ -0,0 +1,17 @@
+import { MongoClient } from 'mongodb';
+const url = 'mongodb://localhost:27017/getorganized';
+let db = null;
+
+export async function connectDb() {
+    if (db) {
+        return db;
+    }
+
+    let client = await MongoClient.connect(url, { useNewUrlParser: true });
+    db = client.db();
+    console.info("Got DB: ", db);
+
+    return db;
+}
+
+//connectDb();
