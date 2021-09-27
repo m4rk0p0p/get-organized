17bc1a1093eff433ec86ce39f03c6a53ec2b87f4
Marko Popovic <m4rk0p0p@gmail.com>
2021-09-27 15:16:23 +0200

    Add functionality to update a task in the database

--- src/server/server.spec.js
+++ src/server/server.spec.js
@@ -1,6 +1,13 @@
-import { addNewTask } from "./server";
+import { addNewTask, updateTask } from "./server";
 
-addNewTask({
-    name: "Dummy task",
-    id: "42"
-});
+(async function asyncAdd() {
+    await addNewTask({
+        name: "Dummy task",
+        id: "42"
+    });
+
+    await updateTask({
+        name: "Dummy task (updated)",
+        id: "42"
+    });
+})();
